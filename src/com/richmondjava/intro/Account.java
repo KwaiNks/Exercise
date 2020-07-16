f94ea1529392f6e86cd59735833d15a9d00ecf3d
richmond <richmond@192.168.0.23>
2020-07-16 04:06:16 +0100

    Account and calculator class

---
+++ src/com/richmondjava/intro/Account.java
@@ -0,0 +1,67 @@
+package com.richmondjava.intro;
+
+import java.util.Scanner;
+
+public class Account {
+
+	//instance variables for Account owner and balance 
+	private String accountOwner;
+	private int balance;
+
+	//constructor to initialize account
+	public Account(String accountOwner, int balance) {
+		this.accountOwner = accountOwner;
+		this.balance = balance;
+	}
+	//get account owner name
+	public String getAccountOwner() {
+		return accountOwner;
+	}
+	//get balance in account
+	public int getBalance() {
+		return balance;
+	} 
+
+	//deposit money in account
+	public int deposit(int amount) {
+		balance+= amount;
+		return balance;
+	}
+	//withdraw money from account
+	public int withdraw(int amount) {
+		balance-= amount;
+		return balance;
+	}
+
+	//main entry into the program
+	public static void main(String[] args) {
+
+		//int variable amount
+		int amount = 0;
+		//creating an instance of Account called myAccount
+		Account myAccount = new Account("David", 1450);
+		//creating an instance of Scanner called scanner
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Welcome! Press 1 for Balance, 2 for Deposit, 3 for Withdraw, 4 to Exit");
+		//Reading input from Scanner and assigning it to numberPressed
+		int numberPressed = scanner.nextInt();	
+		if (numberPressed == 1) {
+			System.out.println(myAccount.getAccountOwner() + " your balance is: " + myAccount.getBalance()); //return balance
+		}else if (numberPressed == 2) {
+			System.out.println("Please enter the amount to be deposited: ");
+			amount = scanner.nextInt();
+			System.out.println(myAccount.accountOwner +" your new balance after the deposit is: " + myAccount.deposit(amount));
+		}else if (numberPressed == 3){
+			System.out.println("Please enter the amount to withdraw: ");
+			amount = scanner.nextInt();
+			System.out.println(myAccount.accountOwner +" your new balance after the withdrawal is: " + myAccount.withdraw(amount));
+		}else if(numberPressed == 4) {
+			System.exit(0);
+		}else {
+			System.out.println("Invalid input ");
+		}
+		scanner.close();
+	}
+
+}
---
+++ src/com/richmondjava/intro/Calculator.java
@@ -0,0 +1,34 @@
+package com.richmondjava.intro;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		double x = 4; //variable assigned to our first value to be added
+		double y = 7; //variable assigned to our second value to be added
+
+		String operation = "/";
+		switch (operation) {
+		case "+":
+			double add = x + y;
+			System.out.printf("The sum of %.2f and %.2f is %.2f ", x, y, add);
+			break;
+		case "-":
+			double sub = x - y;
+			System.out.printf("The subtraction of %.2f and %.2f is %.2f ", x, y, sub);
+			break;
+		case "/":
+			double div = x / y;
+			System.out.printf("The division of %.2f and %.2f is %.2f ", x, y, div);
+			break;
+		case "*":
+			double mult = x * y;
+			System.out.printf("The multiplication of %.2f and %.2f is %.2f ", x, y, mult);
+			break;
+
+		default:
+			System.out.println("Invalid input");
+			break;
+		}	
+}
+}
