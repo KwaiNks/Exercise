c436dd935d3916aa3c2441fe040af1a64f2fbefe
richmond <richmond@192.168.0.23>
2020-07-25 06:32:30 +0100

    improvements done

--- src/com/richmondjava/intro/Account.java
+++ src/com/richmondjava/intro/Account.java
@@ -29,7 +29,11 @@ public class Account {
 	}
 	//withdraw money from account
 	public int withdraw(int amount) {
-		balance-= amount;
+		if (amount > balance) {
+			System.out.println("You cannot withdraw more than what yor balance is.");
+		}else {
+			balance-= amount;
+		}
 		return balance;
 	}
 
@@ -38,30 +42,47 @@ public class Account {
 
 		//int variable amount
 		int amount = 0;
+		int numberPressed;
+
 		//creating an instance of Account called myAccount
 		Account myAccount = new Account("David", 1450);
-		//creating an instance of Scanner called scanner
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Welcome! Press 1 for Balance, 2 for Deposit, 3 for Withdraw, 4 to Exit");
-		//Reading input from Scanner and assigning it to numberPressed
-		int numberPressed = scanner.nextInt();	
-		if (numberPressed == 1) {
-			System.out.println(myAccount.getAccountOwner() + " your balance is: " + myAccount.getBalance()); //return balance
-		}else if (numberPressed == 2) {
-			System.out.println("Please enter the amount to be deposited: ");
-	 		amount = scanner.nextInt();
-			System.out.println(myAccount.accountOwner +" your new balance after the deposit is: " + myAccount.deposit(amount));
-		}else if (numberPressed == 3){
-			System.out.println("Please enter the amount to withdraw: ");
-			amount = scanner.nextInt();
-			System.out.println(myAccount.accountOwner +" your new balance after the withdrawal is: " + myAccount.withdraw(amount));
-		}else if(numberPressed == 4) {
-			System.exit(0); 
-		}else {
-			System.out.println("Invalid input ");
-		}
+		//creating an instance of Scanner called scanner
+		Scanner scanner = new Scanner(System.in);		
+		do {
+			System.out.println("Welcome! Press 1 for Balance, 2 for Deposit, "
+					+ "3 for Withdraw, 4 to Exit");
+
+			//Reading input from Scanner and assigning it to numberPressed
+			numberPressed = scanner.nextInt();	
+
+			if (numberPressed == 1) {
+				System.out.println(myAccount.getAccountOwner() 
+						+ " your balance is: " 
+						+ myAccount.getBalance()); //return balance
+			}
+			else if (numberPressed == 2) {
+				System.out.println("Please enter the amount to be deposited: ");
+				amount = scanner.nextInt();
+				System.out.println(myAccount.getAccountOwner() 
+						+" your new balance after the deposit is: " 
+						+ myAccount.deposit(amount));
+			}
+			else if (numberPressed == 3){
+				System.out.println("Please enter the amount to withdraw: ");
+				amount = scanner.nextInt();
+				System.out.println(myAccount.getAccountOwner() 
+						+" your new balance after the withdrawal is: " 
+						+ myAccount.withdraw(amount));
+			}
+			else if (numberPressed < 1 || numberPressed > 4){
+				System.out.println("Invalid input ");
+			}
+		} while (numberPressed != 4);
+		
+		System.out.println("Bye!");
+		System.exit(0); 
+
 		scanner.close();
 	}
-
 }
