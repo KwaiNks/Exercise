9086fa12e64709b4cbf6e2568eff07ed6471cd1e
richmond <richmond@192.168.0.23>
2020-09-05 09:18:05 +0100

    changed the method signature to String

--- src/com/richmondjava/intro/FizzBuzz.java
+++ src/com/richmondjava/intro/FizzBuzz.java
@@ -2,16 +2,16 @@ package com.richmondjava.intro;
 
 public class FizzBuzz {
 
-	public void getFizzBuzz(int lowerBound, int upperBound) {
+	public String getFizzBuzz(int lowerBound, int upperBound) {
 		for(int i = lowerBound; i <= upperBound; i++) {
 			if (i % 3 == 0) {
-				System.out.println("fizz");
+				return "fizz";
 			}else if (i % 5 == 0) {
-				System.out.println("buzz");
+				return "buzz";
 			}else if ((i % 3 == 0) && (i % 5 == 0)) {
-				System.out.println("fizz-buzz");
+				return "fizz-buzz";
 			}else {
-				System.out.println(i);
+				return String.valueOf(i);
 			}
 		}
 	}
